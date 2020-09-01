e5da0a2bcaac3a860a48e8ded82202947086e90d
tatia-kom <komarovats93@gmail.com>
2020-09-02 02:24:30 +0300

    add index page

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
 
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 0) {
+            $('.header').addClass('header--scroll');
+        }
+        else {
+            $('.header').removeClass('header--scroll');
+        }
+    });
 
+    $('.header-main__menu-button').click(function(e) {
+        e.preventDefault();
+        $(this).toggleClass('header-main__menu-button--active');
+        $('.header-top').toggleClass('header-top--active');
+    });
 
 });
 
